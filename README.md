f7c1cdd3ecfcb2cdaa7ad5d1b3f7704b19e73fe8
nicolasculerier <nicolas.culerier@laposte.net>
2018-07-25 21:45:26 +0200

    Fonctionnel

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 Ruby version : 2.5.1
 Run ```bundle install```
+Run ```rails db:seeds``` to get default database
 
 ##Informations
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Création cours
+
+10.times do |course|
+  course = Course.create(title: Faker::Beer.name, description: Faker::Beer.style)
+end
+
+# Création étudiants
+
+10.times do |student|
+  student = Student.create(first_name: Faker::Name.first_name, last_name:Faker::Name.last_name, course_id: rand(Course.first.id..Course.last.id))
+end
